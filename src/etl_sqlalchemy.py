965eb350d461250ef23e56d63a4ceaafa93b7da5
Emanuel Schemberger <emanueldev24@gmail.com>
2022-11-16 13:46:26 -0300

    unidad 20

---
+++ src/etl_sqlalchemy.py
@@ -0,0 +1,54 @@
+import logging as log
+import pandas as pd
+import sqlalchemy as db
+from sqlalchemy import Integer, Text
+
+# logging config
+log.basicConfig(level=log.DEBUG,
+                format='%(asctime)s %(message)s'
+                )
+
+# sqlalchemy version, engine creation
+try:
+    engine = db.create_engine('sqlite:///datasets/olympics.db')
+    sqla_connection = engine.connect()
+except Exception:
+    log.error("version sqlalchemy connection failed", exc_info=True)
+
+
+# url where dataset is
+url: str = "http://winterolympicsmedals.com/medals.csv "
+
+# Get the data from url
+try:
+    df_medals = pd.read_csv(url)
+except Exception:
+    log.error("url read failed", exc_info=True)
+
+# Filter data and add to a new dataframe
+df_gold_usa = df_medals[(df_medals["NOC"] == "USA") & (df_medals["Medal"]
+                        == "Gold") & (df_medals["Year"] >= 1950)]
+
+# Upload data to "olympics" database in table "medals"
+df_gold_usa.to_sql(
+        'medals',
+        engine,
+        if_exists='replace',
+        index=False,
+        chunksize=500,
+        dtype={
+            "Year": Integer,
+            "City": Text,
+            "Sport": Text,
+            "Discipline":  Text,
+            "NOC": Text,
+            "Event": Text,
+            "Event gender": Text,
+            "Medal": Text
+
+        })
+
+# Select all data from medals table, pandas version
+query = "SELECT * FROM medals"
+data = pd.read_sql(query, con=sqla_connection)
+print(data)
---
+++ src/etl_sqlite.py
@@ -0,0 +1,60 @@
+import logging as log
+import pandas as pd
+import sqlite3
+
+# logging config
+log.basicConfig(level=log.DEBUG,
+                format='%(asctime)s %(message)s'
+                )
+
+# sqlite connection version
+try:
+    conn = sqlite3.connect('datasets/olympics.db')
+    # cursor
+    cursor = conn.cursor()
+except Exception:
+    log.error("version sqlite connection failed", exc_info=True)
+
+# url where dataset is
+url: str = "http://winterolympicsmedals.com/medals.csv "
+
+# Get the data from url
+try:
+    df_medals = pd.read_csv(url)
+except Exception:
+    log.error("url read failed", exc_info=True)
+
+
+# Filter data and add to a new dataframe
+df_gold_usa = df_medals[(df_medals["NOC"] == "USA") & (df_medals["Medal"]
+                        == "Gold") & (df_medals["Year"] >= 1950)]
+
+# Create table medals2
+query = """CREATE TABLE IF NOT EXISTS medals2 (
+            Year INTEGER,
+            City TEXT,
+            Sport TEXT,
+            Discipline TEXT,
+            NOC TEXT,
+            Event TEXT,
+            Event_gender TEXT,
+            Medal TEXT
+            )"""
+cursor.execute(query)
+conn.commit()
+
+# Insert
+df_gold_usa.to_sql(
+        'medals2',
+        conn,
+        if_exists='replace',
+        index=False,
+        chunksize=500,
+        )
+
+# select all data from medals, sqlite version
+query2 = "SELECT * FROM medals2"
+cursor.execute(query2)
+one_result = cursor.fetchall()
+# one_result = pd.DataFrame(one_result)
+print(one_result)
